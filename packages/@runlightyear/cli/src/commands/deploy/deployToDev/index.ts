c2a8179074dbfbf30d727c2aea986e02297abb26
Eric Bouck <eric.bouck@gmail.com>
2022-11-11 14:18:47 -0800

    Don't use subscribe / unsubscribe for now

--- packages/@runlightyear/cli/src/commands/deploy/deployToDev/index.ts
+++ packages/@runlightyear/cli/src/commands/deploy/deployToDev/index.ts
@@ -5,6 +5,6 @@ import subscribe from "../../../shared/subscribe";
 export default async function index() {
   console.log("ready to deploy to dev");
   await execDeploy();
-  await unsubscribe();
-  await subscribe();
+  // await unsubscribe();
+  // await subscribe();
 }
