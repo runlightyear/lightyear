ada8ca791b74d272340697fbd187e8f079d4da36
Eric Bouck <eric.bouck@gmail.com>
2023-02-21 11:45:12 -0800

    Fixed broken link in footer

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             items: [
               {
                 label: "API",
-                to: "/docs",
+                to: "/docs/api",
               },
             ],
           },
