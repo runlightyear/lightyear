32f10416ac01e4dac8f2bd7e0d80a96dc9138a3a
Eric Bouck <eric.bouck@gmail.com>
2023-10-02 09:48:04 -0700

    Fixed getTree

--- packages/@runlightyear/github/src/gitDatabase/trees/getTree.ts
+++ packages/@runlightyear/github/src/gitDatabase/trees/getTree.ts
@@ -2,23 +2,6 @@ import { HttpProxyResponse } from "@runlightyear/lightyear";
 import { Tree } from "../../types/Tree";
 import { GitHub } from "../../GitHub";
 
-/**
- * Path parameters
- * Name, Type, Description
- * owner string Required
- * The account owner of the repository. The name is not case sensitive.
- *
- * repo string Required
- * The name of the repository without the .git extension. The name is not case sensitive.
- *
- * tree_sha string Required
- * The SHA1 value or ref (branch or tag) name of the tree.
- *
- * Query parameters
- * Name, Type, Description
- * recursive string
- * Setting this parameter to any value returns the objects or subtrees referenced by the tree specified in :tree_sha. For example, setting recursive to any of the following will enable returning objects or subtrees: 0, 1, "true", and "false". Omit this parameter to prevent recursively returning objects or subtrees.
- */
 export interface GetTreeProps {
   /**
    * The account owner of the repository. The name is not case sensitive.
@@ -38,13 +21,13 @@ export interface GetTreeProps {
   recursive?: string | boolean | number;
 }
 
-export interface TreeResponse extends HttpProxyResponse {
+export interface GetTreeResponse extends HttpProxyResponse {
   data: Tree;
 }
 
 export const getTree =
   (self: GitHub) =>
-  async (props: GetTreeProps): Promise<TreeResponse> => {
+  async (props: GetTreeProps): Promise<GetTreeResponse> => {
     const { owner, repo, treeSha, recursive } = props;
 
     return await self.get({
