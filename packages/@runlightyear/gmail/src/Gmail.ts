e79e790c8da899486b8e6cd9837534fee2cd82bb
Eric Bouck <eric.bouck@gmail.com>
2023-10-28 10:04:09 -0700

    Gmail uses getBaseUrl and getDefaultHeaders

--- packages/@runlightyear/gmail/src/Gmail.ts
+++ packages/@runlightyear/gmail/src/Gmail.ts
@@ -1,15 +1,27 @@
-import { AuthConnectorProps, RestConnector } from "@runlightyear/lightyear";
+import { RestConnector, RestConnectorProps } from "@runlightyear/lightyear";
 
 /**
  * @alpha
  */
-export interface GmailProps extends AuthConnectorProps {}
+export interface GmailProps extends RestConnectorProps {}
 
 /**
  * @alpha
  */
 export class Gmail extends RestConnector {
   constructor(props: GmailProps) {
-    super({ ...props, baseUrl: "https://gmail.googleapis.com" });
+    super(props);
+  }
+
+  getBaseUrl() {
+    return "https://gmail.googleapis.com";
+  }
+
+  getDefaultHeaders() {
+    const { accessToken } = this.getAuthData();
+
+    return {
+      Authorization: `Bearer ${accessToken}`,
+    };
   }
 }
