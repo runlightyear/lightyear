9f85bc268c72277462f94b48bf1244f7ac968b4f
Eric Bouck <eric.bouck@gmail.com>
2024-01-25 10:22:07 -0800

    Back out receive webhook code for now

--- packages/@runlightyear/github/src/deliveries/receiveDelivery.ts
+++
@@ -1,5 +0,0 @@
-import { GitHub } from "../GitHub";
-import { WebhookDeliveryData } from "@runlightyear/lightyear";
-
-export const receiveDelivery =
-  (self: GitHub) => async (props: WebhookDeliveryData) => {};
