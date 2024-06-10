0d3115e88c14e202f3b9671aedfa48bdeb948a5a
Eric Bouck <eric.bouck@gmail.com>
2024-06-10 13:44:41 -0700

    LY-1406 HubSpot uses new sync integration definition

--- packages/@runlightyear/hubspot/src/synchronizers/AccountSynchronizer.ts
+++ packages/@runlightyear/hubspot/src/synchronizers/AccountSynchronizer.ts
@@ -1,96 +1,40 @@
-import { HubSpot } from "../HubSpot";
 import {
-  AuthConnector,
-  ModelSynchronizer,
-  ModelSynchronizerProps,
-} from "@runlightyear/lightyear";
-import {
-  CreateObjectProps,
-  ObjectId,
-  UpdateObjectProps,
-} from "@runlightyear/lightyear/src/synchronizers/ModelSynchronizer";
+  HubSpotModelSynchronizer,
+  HubSpotModelSynchronizerProps,
+} from "./HubSpotModelSynchronizer";
 
-export interface AccountSyncProps extends ModelSynchronizerProps {
-  hubspot: HubSpot;
-}
+export interface AccountSynchronizerProps
+  extends HubSpotModelSynchronizerProps {}
 
-export class AccountSynchronizer<T = any> extends ModelSynchronizer<T> {
-  hubspot: HubSpot;
+// force recompile
 
-  constructor(props: AccountSyncProps) {
-    super(props);
-    this.hubspot = props.hubspot;
+export class AccountSynchronizer extends HubSpotModelSynchronizer {
+  constructor(props: AccountSynchronizerProps) {
+    const { model = "account", ...rest } = props;
+    super({ model, ...rest });
   }
 
-  getConnector(): AuthConnector {
-    return this.hubspot;
+  getNoun(): string {
+    return "company";
   }
 
-  getDefaultToObject(): { [p: string]: string | ((source: any) => any) } {
+  getPluralNoun(): string {
+    return "companies";
+  }
+
+  getToObjectData(): any {
     return {
-      name: "name",
-      domain: "domain",
+      ...super.getToObjectData(),
+      name: "properties.name",
+      domain: "properties.domain",
     };
   }
 
-  getDefaultToSource(): { [p: string]: string | ((model: any) => any) } {
+  getFromObjectData(): any {
     return {
+      ...super.getFromObjectData(),
       name: "name",
       domain: "domain",
     };
-  }
-
-  async list() {
-    const response = await this.hubspot.get({
-      url: "/crm/v3/objects/companies",
-    });
-
-    return response.data.results.map((result: any) => ({
-      id: result.id,
-      updatedAt: result.updatedAt,
-      data: this.mapToObject(result.properties),
-    }));
-  }
-
-  async get(id: string) {
-    const response = await this.hubspot.get({
-      url: `/crm/v3/objects/companies/${id}`,
-    });
-
-    return {
-      id: response.data.id,
-      updatedAt: response.data.updatedAt,
-      data: {
-        properties: this.mapToObject(response.data),
-      },
-    } as any;
-  }
-
-  async create(obj: CreateObjectProps<T>) {
-    const response = await this.hubspot.post({
-      url: "/crm/v3/objects/companies",
-      data: {
-        properties: this.mapToSource(obj.data),
-      },
-    });
-
-    return response.data.id;
-  }
-
-  async update(obj: UpdateObjectProps<T>) {
-    const response = await this.hubspot.patch({
-      url: `/crm/v3/objects/companies/${obj.id}`,
-      data: {
-        properties: this.mapToSource(obj.data),
-      },
-    });
-
-    return response.data.id;
-  }
-
-  async delete(id: ObjectId) {
-    await this.hubspot.delete({
-      url: `/crm/v3/objects/companies/${id}`,
-    });
   }
 }
--- packages/@runlightyear/hubspot/src/synchronizers/HubSpotSynchronizer.ts
+++ packages/@runlightyear/hubspot/src/synchronizers/HubSpotSynchronizer.ts
@@ -3,75 +3,36 @@ import {
   CollectionSynchronizer,
   CollectionSynchronizerProps,
   ModelSynchronizer,
+  ModelSynchronizerProps,
 } from "@runlightyear/lightyear";
 import { ContactSynchronizer } from "./ContactSynchronizer";
 import { AccountSynchronizer } from "./AccountSynchronizer";
 
-export interface HubSpotSynchronizerProps extends CollectionSynchronizerProps {
-  hubspot: HubSpot;
+export interface HubSpotSynchronizerProps
+  extends Omit<CollectionSynchronizerProps, "collection"> {
+  collection?: string;
 }
 
 export class HubSpotSynchronizer extends CollectionSynchronizer {
   hubspot: HubSpot;
 
   constructor(props: HubSpotSynchronizerProps) {
-    const { hubspot, models = [] } = props;
-    super(props);
-    this.hubspot = hubspot;
+    const { collection = "crm", ...rest } = props;
+    super({ collection, ...rest });
+
+    if (props.connector instanceof HubSpot) {
+      this.hubspot = props.connector;
+    } else {
+      throw new Error("HubSpotSynchronizer requires a HubSpot connector");
+    }
   }
 
-  async getModel(name: string) {
-    if (!(name in this.models)) {
-      throw new Error(`Model not configured: ${name}`);
-    }
-
-    const modelDef = this.models[name];
-
-    console.log("modelDef", modelDef);
-
-    switch (name) {
-      case "account": {
-        if (modelDef === true) {
-          return new AccountSynchronizer({
-            hubspot: this.hubspot,
-            collection: this.collection,
-            model: "account",
-          });
-        } else if (modelDef instanceof ModelSynchronizer) {
-          return modelDef;
-        } else {
-          return new AccountSynchronizer({
-            hubspot: this.hubspot,
-            collection: this.collection,
-            model: "account",
-            ...modelDef,
-          });
-        }
-      }
-      case "contact": {
-        if (modelDef === true) {
-          return new ContactSynchronizer({
-            hubspot: this.hubspot,
-            collection: this.collection,
-            model: "contact",
-          });
-        } else if (modelDef instanceof ModelSynchronizer) {
-          return modelDef;
-        } else {
-          return new ContactSynchronizer({
-            hubspot: this.hubspot,
-            collection: this.collection,
-            model: "contact",
-            ...modelDef,
-          });
-        }
-      }
-      default: {
-        if (modelDef instanceof ModelSynchronizer) {
-          return modelDef;
-        }
-        throw new Error(`Model not supported: ${name}`);
-      }
-    }
+  getDefaultModelSynchronizers(): {
+    [p: string]: (props: ModelSynchronizerProps) => ModelSynchronizer<any>;
+  } {
+    return {
+      account: (props) => new AccountSynchronizer(props),
+      contact: (props) => new ContactSynchronizer(props),
+    };
   }
 }
