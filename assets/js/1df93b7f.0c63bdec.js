7a33f00aaef989ed44ac96d7db318a68cbb469f7
ebouck <ebouck@users.noreply.github.com>
2024-06-14 17:47:44 +0000

    deploy: 0b98a0de94a73b38987d68cd8de71d6c7890c7f5

---
+++ assets/js/1df93b7f.0c63bdec.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[53237],{23072:(e,c,s)=>{s.r(c),s.d(c,{default:()=>n});var u=s(29901);function n(){return(0,u.useEffect)((()=>{window.location.href="/docs/api"}),[]),null}}}]);
