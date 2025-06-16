47a44a9b858d2d93674125d08f9b5a70901e1e7b
Eric Bouck <eric.bouck@gmail.com>
2025-06-16 13:20:32 -0700

    Support basic auth

--- packages/@runlightyear/lightyear/src/connectors/RestConnector.ts
+++ packages/@runlightyear/lightyear/src/connectors/RestConnector.ts
@@ -46,12 +46,21 @@ export abstract class RestConnector extends AuthConnector {
   }
 
   getDefaultHeaders(): Record<string, any> {
-    const { accessToken } = this.getAuthData();
+    const { accessToken, username, password } = this.getAuthData();
+
+    let authorizationHeader = undefined;
+    if (accessToken) {
+      authorizationHeader = `Bearer ${accessToken}`;
+    } else if (username && password) {
+      authorizationHeader = `Basic ${Buffer.from(
+        `${username}:${password}`
+      ).toString("base64")}`;
+    }
 
     return {
       "Content-Type": "application/json",
       Accept: "application/json",
-      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+      Authorization: authorizationHeader,
     };
   }
 
